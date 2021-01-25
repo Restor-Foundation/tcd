d9f13b994c1c16f1ebffb1213a8a674805c84e27
Andrew Cottam <a.cottam@gmail.com>
2021-01-25 12:34:13 +0000

    Updates

---
+++ precache-manifest.520623023e7d53a912bca923c41fc9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d697e48505709089986e730b30c18075",
+    "url": "/tcd/index.html"
+  },
+  {
+    "revision": "435618bbacab9639dc42",
+    "url": "/tcd/static/css/2.945747aa.chunk.css"
+  },
+  {
+    "revision": "435618bbacab9639dc42",
+    "url": "/tcd/static/js/2.f40b3ff7.chunk.js"
+  },
+  {
+    "revision": "8a3d619c2c556f68d686f15292bc8611",
+    "url": "/tcd/static/js/2.f40b3ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46dfd81ef558d2b1ea2b",
+    "url": "/tcd/static/js/main.44d5a5e5.chunk.js"
+  },
+  {
+    "revision": "f8342622df0d8e6f46c9",
+    "url": "/tcd/static/js/runtime-main.643607e0.js"
+  }
+]);

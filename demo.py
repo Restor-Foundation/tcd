5a82d6ab4a5de6b11bb608dcf14ba6f572c4b4b8
Jasper <jdekoninck@student.ethz.ch>
2022-10-20 00:07:38 +0200

    post processing

--- demo.py
+++ demo.py
@@ -1,15 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
+import torch
+
+torch.cuda.is_available = lambda: False
 
 import model
+from general_statistics import Statistics
+from post_processing import PostProcessor
 
 runner = model.ModelRunner("default.yaml")
 image_path = "./data/5c15321f63d9810007f8b06f_10_00000.tif"
+post_processor = PostProcessor(runner.config)
+stat_calculator = Statistics()
 
-results = runner.detect_tiled(image_path, tile_size=512, pad=100, skip_empty=True)
+results = runner.detect_tiled(image_path, tile_size=1024, pad=100, skip_empty=True)
+
 
 image = rasterio.open(image_path)
+
+processed_result = post_processor.process_tiled_result(results, image, treshold=0.5)
+general_stats, tree_stats = stat_calculator.run(processed_result)
+
+# You can also just visualize the processed result, commented for now
+# processed_result.visualize(figsize=(15, 15))
+print(general_stats)
+print(tree_stats.head())
 
 plt.figure(figsize=(15, 15))
 
@@ -44,4 +60,4 @@ plt.imshow(masked, alpha=0.8, extent=extent, cmap="Blues_r")
 masked = np.ma.masked_where(image_mask[:, :, 1] == 0, image_mask[:, :, 1])
 plt.imshow(masked, alpha=0.8, extent=extent, cmap="Reds_r")
 
-plt.savefig("5c15321f63d9810007f8b06f_10_00000.png")
+plt.savefig("output/5c15321f63d9810007f8b06f_10_00000.png")

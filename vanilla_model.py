d9622b50b861c0557cd0e7e0b24d1bd66e1474e9
Benjamin Dayan <benjamin.l.dayan@gmail.com>
2022-11-01 11:00:36 +0100

    Fix up image segmentation logging to wandb. Also model checkpoints to wandb are working

--- vanilla_model.py
+++ vanilla_model.py
@@ -1,43 +1,52 @@
-import configparser
-from ctypes import cast
-import os
-import json
-from pathlib import Path
 import argparse
+import configparser
+import json
+import os
+import sys
+import time
+import warnings
+from ctypes import cast
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
-from decouple import config
-
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
 import torch
 from torch.utils.data import DataLoader, Dataset
 
 import torchgeo
-from torchgeo.trainers import SemanticSegmentationTask
+from torchgeo.datasets.utils import unbind_samples
 
 import yaml
-import configparser
-import sys
-
-from pytorch_lightning import Trainer
+from decouple import config
+from PIL import Image
+from pytorch_lightning import Trainer, LightningDataModule
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.loggers import CSVLogger, WandbLogger
+from torch.utils.data import DataLoader, Dataset
+from torchgeo.trainers import SemanticSegmentationTask
+from torchmetrics import (
+    Accuracy,
+    ConfusionMatrix,
+    F1Score,
+    JaccardIndex,
+    MetricCollection,
+    Precision,
+    Recall,
+)
 
+import wandb
 
-DATA_DIR = config('DATA_DIR')
-REPO_DIR = config('REPO_DIR')
+# TODO fix warnings
+warnings.filterwarnings("ignore")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--conf", type=str, nargs='?', const=True, default='conf.yaml',
-                    help="Choose config file for setup")
-args = parser.parse_args()
+DATA_DIR = config("DATA_DIR")
+LOG_DIR = config("LOG_DIR")
+REPO_DIR = config("REPO_DIR")
 
-conf = configparser.ConfigParser()
-conf.read(args.conf)
-
-if conf['experiment']['setup'] == 'True':
-    from utils import clean_data
 
 
 # if the imports throw OMP error #15, try $ conda install nomkl
@@ -47,13 +56,11 @@ if conf['experiment']['setup'] == 'True':
 class ImageDataset(Dataset):
     def __init__(self, setname, transform=None, target_transform=None):
 
-        # Define the  mask file and the json file for retrieving images
-        # self.data_dir = os.getcwd()
         self.data_dir = DATA_DIR
         self.setname = setname
         assert setname in ["train", "test", "val"]
 
-        with open(self.data_dir + setname + "_20221010.json", 'r') as file:
+        with open(self.data_dir + setname + "_20221010.json", "r") as file:
             self.metadata = json.load(file)
 
         self.transform = transform
@@ -63,7 +70,7 @@ class ImageDataset(Dataset):
         return len(list(self.metadata.items())[2][1])
 
     def __getitem__(self, idx):
-        img_name = list(self.metadata.items())[2][1][idx]["file_name"]
+        img_name = self.metadata['images'][idx]["file_name"]
         img_path = os.path.join(self.data_dir, "images", img_name)
         try:
             image = torch.Tensor(np.array(Image.open(img_path)))
@@ -73,7 +80,7 @@ class ImageDataset(Dataset):
 
         mask = np.load(
             self.data_dir + "masks/" + self.setname + "_mask_" + str(idx) + ".npz"
-        )['arr_0'].astype(int)
+        )["arr_0"].astype(int)
 
         if self.transform:
             image = self.transform(image)
@@ -81,11 +88,237 @@ class ImageDataset(Dataset):
             mask = self.target_transform(mask)
         return {"image": image, "mask": mask}
 
+
+class TreeDataModule(LightningDataModule):
+    def __init__(self, conf, data_frac=1.0):
+        super().__init__()
+        self.conf = conf
+        self.data_frac = data_frac
+    def prepare_data(self) -> None:
+        self.train_data, self.val_data, self.test_data = ImageDataset("train"), ImageDataset("val"), ImageDataset("test")
+
+    def train_dataloader(self):
+        return get_dataloaders(self.conf, self.train_data, data_frac=self.data_frac)[0]
+    def val_dataloader(self):
+        return get_dataloaders(self.conf, self.val_data, data_frac=self.data_frac)[0]
+    def test_dataloader(self):
+        return get_dataloaders(self.conf, self.test_data, data_frac=self.data_frac)[0]
+
+    def plot(self, sample):
+        img, mask, pred = sample['image'], sample['mask'], sample['prediction']
+        # pred_mask = pred.argmax(axis=0)
+        WBMASK = wb_mask(img.cpu().numpy().transpose(1, 2, 0), pred.cpu().numpy(), mask.cpu().numpy())
+        return WBMASK
+
 def collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
     return torch.utils.data.dataloader.default_collate(batch)
 
+
+class SemanticSegmentationTaskPlus(SemanticSegmentationTask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.train_metrics = MetricCollection(
+            [
+                Accuracy(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                    mdmc_average="global",
+                ),
+                JaccardIndex(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                ),
+                Precision(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                    mdmc_average="global",
+                ),
+                Recall(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                    mdmc_average="global",
+                ),
+                F1Score(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                    mdmc_average="global",
+                ),
+                ConfusionMatrix(
+                    num_classes=self.hyperparams["num_classes"],
+                    ignore_index=self.ignore_index,
+                ),
+            ],
+            prefix="train_",
+        )
+        self.val_metrics = self.train_metrics.clone(prefix="val_")
+        self.test_metrics = self.train_metrics.clone(prefix="test_")
+
+    def validation_step(self, *args, **kwargs) -> None:
+        """Compute validation loss and log example predictions.
+
+        Args:
+            batch: the output of your DataLoader
+            batch_idx: the index of this batch
+        """
+        batch = args[0]
+        batch_idx = args[1]
+        x = batch["image"]
+        y = batch["mask"]
+        y_hat = self.forward(x)
+        y_hat_hard = y_hat.argmax(dim=1)
+
+        loss = self.loss(y_hat, y)
+
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
+        self.val_metrics(y_hat_hard, y)
+
+        if batch_idx < 10:
+            try:
+                datamodule = self.trainer.datamodule  # type: ignore[attr-defined]
+                batch["prediction"] = y_hat_hard
+                for key in ["image", "mask", "prediction"]:
+                    batch[key] = batch[key].cpu()
+                sample = unbind_samples(batch)[0]
+                fig = datamodule.plot(sample)
+                summary_writer = self.logger.experiment  # type: ignore[union-attr]
+                summary_writer.log({'predictions': fig})
+            except AttributeError:
+                pass
+
+    def training_epoch_end(self, outputs):
+        """Logs epoch level training metrics.
+
+        Args:
+            outputs: list of items returned by training_step
+        """
+        computed = self.train_metrics.compute()
+        conf_mat = computed["train_ConfusionMatrix"].cpu().numpy()
+        conf_mat = (conf_mat / np.sum(conf_mat)) * 100
+        df_cm = pd.DataFrame(
+            conf_mat,
+            index=range(self.hyperparams["num_classes"]),
+            columns=range(self.hyperparams["num_classes"]),
+        )
+        new_metrics = {
+            k: computed[k] for k in set(list(computed)) - set(["train_ConfusionMatrix"])
+        }
+        fig = px.imshow(conf_mat, text_auto=".2f")
+        wandb.log({"train_ConfusionMatrix": wandb.Table(dataframe=df_cm)})
+        wandb.log({"train_cm": fig})
+        self.log_dict(new_metrics)
+        self.train_metrics.reset()
+
+    def validation_epoch_end(self, outputs):
+        """Logs epoch level validation metrics.
+
+        Args:
+            outputs: list of items returned by validation_step
+        """
+        computed = self.val_metrics.compute()
+        conf_mat = computed["val_ConfusionMatrix"].cpu().numpy()
+        conf_mat = (conf_mat / np.sum(conf_mat)) * 100
+        df_cm = pd.DataFrame(
+            conf_mat,
+            index=range(self.hyperparams["num_classes"]),
+            columns=range(self.hyperparams["num_classes"]),
+        )
+        new_metrics = {
+            k: computed[k] for k in set(list(computed)) - set(["val_ConfusionMatrix"])
+        }
+        fig = px.imshow(conf_mat, text_auto=".2f")
+        wandb.log({"val_ConfusionMatrix": wandb.Table(dataframe=df_cm)})
+        wandb.log({"val_cm": fig})
+        self.log_dict(new_metrics)
+        self.val_metrics.reset()
+
+    def test_epoch_end(self, outputs):
+        """Logs epoch level test metrics.
+
+        Args:
+            outputs: list of items returned by test_step
+        """
+        computed = self.test_metrics.compute()
+        conf_mat = computed["test_ConfusionMatrix"].cpu().numpy()
+        conf_mat = (conf_mat / np.sum(conf_mat)) * 100
+        df_cm = pd.DataFrame(
+            conf_mat,
+            index=range(self.hyperparams["num_classes"]),
+            columns=range(self.hyperparams["num_classes"]),
+        )
+        new_metrics = {
+            k: computed[k] for k in set(list(computed)) - set(["test_ConfusionMatrix"])
+        }
+        fig = px.imshow(conf_mat, text_auto=".2f")
+        wandb.log({"test_ConfusionMatrix": wandb.Table(dataframe=df_cm)})
+        wandb.log({"test_cm": fig})
+        self.log_dict(new_metrics)
+        self.test_metrics.reset()
+
+
+
+
+def get_dataloaders(conf, *datasets, data_frac=1.0):
+    if data_frac != 1.0:
+        datasets = [
+            torch.utils.data.Subset(dataset, np.random.choice(len(dataset), int(len(dataset) * data_frac), replace=False))
+            for dataset in datasets
+        ]
+
+    return [
+        DataLoader(
+            dataset,
+            batch_size=int(conf["datamodule"]["batch_size"]),
+            shuffle=True,
+            num_workers=int(conf["datamodule"]["num_workers"]),
+            collate_fn=collate_fn,
+        )
+        for dataset in datasets
+    ]
+
+try:
+    conf = configparser.ConfigParser()
+    conf.read("conf.yaml")
+except Exception as e:
+    pass  # no conf
+
+
+segmentation_classes = [
+    'no tree', 'tree'
+]
+
+def labels():
+  l = {}
+  for i, label in enumerate(segmentation_classes):
+    l[i] = label
+  return l
+
+def wb_mask(bg_img, pred_mask, true_mask):
+  return wandb.Image(bg_img, masks={
+    "prediction" : {"mask_data" : pred_mask, "class_labels" : labels()},
+    "ground truth" : {"mask_data" : true_mask, "class_labels" : labels()}})
+
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--conf",
+        type=str,
+        nargs="?",
+        const=True,
+        default="conf.yaml",
+        help="Choose config file for setup",
+    )
+    args = parser.parse_args()
+
+    conf = configparser.ConfigParser()
+    conf.read(args.conf)
+
+    if conf["experiment"]["setup"] == "True":
+        from utils import clean_data
+
+    wandb.init(entity="dsl-ethz-restor", project="vanilla-model-more-metrics")
 
     # create datasets
     setname = "train"
@@ -104,26 +337,42 @@ if __name__ == "__main__":
                                  shuffle=False, num_workers=int(conf['datamodule']['num_workers']),collate_fn=collate_fn)
     
 
-    experiment_dir = os.path.join('REPO_DIR', "results")
-    checkpoint_callback = ModelCheckpoint(monitor="val_loss", dirpath=experiment_dir, 
-                                          save_top_k=1, save_last=True)
-    early_stopping_callback = EarlyStopping(monitor="val_loss", min_delta=0.00, patience=10)
-    csv_logger = CSVLogger(save_dir=experiment_dir, name="logs")
+    # checkpoints and loggers
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        dirpath=LOG_DIR + "/checkpoints",
+        save_top_k=1,
+        save_last=True,
+    )
+    early_stopping_callback = EarlyStopping(
+        monitor="val_loss", min_delta=0.00, patience=10
+    )
+    csv_logger = CSVLogger(save_dir=LOG_DIR, name="logs")
+    wandb_logger = WandbLogger(project="vanilla-model-more-metrics", log_model="all")
 
-    # set up task
-    task = SemanticSegmentationTask(
+    # task
+    task = SemanticSegmentationTaskPlus(
+        segmentation_model=conf["model"]["segmentation_model"],
+        encoder_name=conf["model"]["backbone"],
+        encoder_weights="imagenet" if conf["model"]["pretrained"] == "True" else "None",
+        in_channels=int(conf["model"]["in_channels"]),
+        num_classes=int(conf["model"]["num_classes"]),
+        loss=conf["model"]["loss"],
+        ignore_index=None,
+        learning_rate=float(conf["model"]["learning_rate"]),
+        learning_rate_schedule_patience=int(
+            conf["model"]["learning_rate_schedule_patience"]
+        ),
+    )
+
+    # trainer
+    trainer = Trainer(
         callbacks=[checkpoint_callback, early_stopping_callback],
-        logger=[csv_logger],
-        default_root_dir=experiment_dir,
-        segmentation_model=conf['model']['segmentation_model'],
-        encoder_name= conf['model']['backbone'],
-        encoder_weights= 'imagenet' if conf['model']['pretrained'] == 'True' else 'None',
-        in_channels=int(conf['model']['in_channels']),
-        num_classes=int(conf['model']['num_classes']),
-        loss= conf['model']['loss'],
-        ignore_index= None,
-        learning_rate= float(conf['model']['learning_rate']),
-        learning_rate_schedule_patience=int(conf['model']['learning_rate_schedule_patience']),
+        logger=[csv_logger, wandb_logger],
+        default_root_dir=LOG_DIR,
+        accelerator="gpu",
+        max_epochs=int(conf["trainer"]["max_epochs"]),
+        max_time="00:23:50:00",
     )
 
     trainer = Trainer(accelerator="gpu", max_epochs=int(conf['trainer']['max_epochs']), max_time="00:23:50:00") 

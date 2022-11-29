c43a7c4eb4f37c49168e08fc69581a8986b45879
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2022-11-29 13:49:16 +0100

    working cache + bugfixes

--- demo_predict_large.py
+++ demo_predict_large.py
@@ -4,10 +4,17 @@ import rasterio
 
 from modelrunner import ModelRunner
 
-runner = ModelRunner("default_tta.yaml")
+runner = ModelRunner("default.yaml")
 
 image_path = "./data/5f058f16ce2c9900068d83ed.tif"
-results = runner.predict(image_path, tiled=True, tile_size=1024, overlap=100)
+results = runner.predict(image_path, tiled=True, tile_size=512, overlap=100)
+
+"""
+from post_processing import PostProcessor
+
+process = PostProcessor(runner.config, image=rasterio.open(image_path))
+process.process_cached()
+results = process.process_tiled_result()
+"""
 
 results.serialise("./data/5f058f16ce2c9900068d83ed_pred", image_path=image_path)
-# results.visualise()

71ef5b342b8ba381fbaf3a9aee038b9e0968b8d3
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2022-11-30 01:19:12 +0100

    resizer script

---
+++ resize_image.py
@@ -0,0 +1,19 @@
+import argparse
+import logging
+
+from utils import convert_to_projected
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, help="Input image", required=True)
+    parser.add_argument("--gsd", type=float, help="Ground sample distance", default=0.1)
+
+    args = parser.parse_args()
+
+    convert_to_projected(
+        args.input, inplace=False, resample=True, target_gsd_m=args.gsd
+    )

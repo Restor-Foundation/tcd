0f3a27bf1f12c6e3ccf94ba02b60da5e078197bb
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2022-12-12 10:20:18 +0100

    black

--- tests/test_modelrunner.py
+++ tests/test_modelrunner.py
@@ -9,16 +9,34 @@ def test_load_detectron():
     assert runner.config is not None
 
 
-def test_load_torchgeo():
-    """Test if we can load a basic configuration"""
-    runner = ModelRunner("./config/base_detectron.yaml")
-    assert runner.config is not None
-
-
 def test_predict_simple():
     """Test if we can perform simple prediction"""
     runner = ModelRunner("./config/base_detectron.yaml")
     results = runner.predict(test_image_path, tiled=False)
+    assert len(results.get_trees()) > 0
+
+
+def test_predict_tiled_coco():
+    """Test if we can perform simple prediction"""
+    runner = ModelRunner("./config/base_detectron.yaml")
+    runner.model.post_processor.cache_format = "coco"
+    results = runner.predict(test_image_path, tiled=True)
+    assert len(results.get_trees()) > 0
+
+
+def test_predict_tiled_numpy():
+    """Test if we can perform simple prediction"""
+    runner = ModelRunner("./config/base_detectron.yaml")
+    runner.model.post_processor.cache_format = "numpy"
+    results = runner.predict(test_image_path, tiled=True)
+    assert len(results.get_trees()) > 0
+
+
+def test_predict_tiled_pickle():
+    """Test if we can perform simple prediction"""
+    runner = ModelRunner("./config/base_detectron.yaml")
+    runner.model.post_processor.cache_format = "pickle"
+    results = runner.predict(test_image_path, tiled=True)
     assert len(results.get_trees()) > 0
 
 

e85820100a568c2b38a448ca6fc0b394aa10298f
Josh Veitch-Michaelis <josh@restor.eco>
2023-01-13 10:06:29 +0000

    download assets

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,3 +4,13 @@ conda env create -f ./docker/environment.yaml
 conda activate tcd
 pip install torchmetrics==0.10.3
 pip install -e .[test]
+
+# Download data + checkpoints and extract
+gh release download init
+mkdir -p checkpoints
+mv *.ckpt checkpoints
+mv *.pth checkpoints
+
+mkdir -p data
+mv restor-tcd-oam* data
+cd data && cat restor-tcd-oam-20221010.tar.gz.* | tar xzvf - && cd ..

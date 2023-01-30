b41eb3b14e70f8ae6111f08f5da35a77218a14aa
Josh Veitch-Michaelis <josh@restor.eco>
2023-01-30 07:02:34 +0000

    add local docker build script

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf ./docker-tcd-pipeline
+git clone git@github.com:Restor-Foundation/tcd-pipeline.git docker-tcd-pipeline --depth 1 --single-branch --branch main
+rm -rf docker-tcd-pipeline/.git
+cp -r ../checkpoints ./checkpoints
+docker build -t tcd .
+rm -rf ./docker-tcd-pipeline
+rm -rf ./checkpoints

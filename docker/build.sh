53ec98b53f5cb00deeae97fc44b0fcea5f27f5ad
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2023-01-30 07:56:35 +0000

    add arm dockerfile, update build script

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ rm -rf ./docker-tcd-pipeline
 git clone git@github.com:Restor-Foundation/tcd-pipeline.git docker-tcd-pipeline --depth 1 --single-branch --branch post_processing_refactor
 rm -rf docker-tcd-pipeline/.git
 cp -r ../checkpoints ./checkpoints
-docker build -t tcd .
+docker build -t tcd . -f $1
 rm -rf ./docker-tcd-pipeline
 rm -rf ./checkpoints

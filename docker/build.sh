6d47d08b7d7200e0ec53ae58b43788a48a9f44cb
Josh Veitch-Michaelis <josh@restor.eco>
2023-01-30 07:05:11 +0000

    switch branch

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ./docker-tcd-pipeline
-git clone git@github.com:Restor-Foundation/tcd-pipeline.git docker-tcd-pipeline --depth 1 --single-branch --branch main
+git clone git@github.com:Restor-Foundation/tcd-pipeline.git docker-tcd-pipeline --depth 1 --single-branch --branch post-processing-refactor
 rm -rf docker-tcd-pipeline/.git
 cp -r ../checkpoints ./checkpoints
 docker build -t tcd .

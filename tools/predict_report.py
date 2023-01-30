08752551795038e1c8588d0cf58214b87202b2e6
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2023-01-30 17:28:27 +0100

    update report script

--- tools/predict_report.py
+++ tools/predict_report.py
@@ -12,9 +12,10 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def predict_and_serialise(image, config, serialise_path, warm=False):
+def predict_and_serialise(image, config, serialise_path, gsd_m=0.1, warm=False):
     tstart = time.time()
     runner = ModelRunner(config)
+    runner.config.data.gsd = gsd_m
     results = runner.predict(image, warm_start=warm)
     tend = time.time()
     results.prediction_time_s = tend - tstart
@@ -36,24 +37,30 @@ def main(args):
         if not os.path.exists(resampled_image_path):
             logger.info("Resampling")
 
-            convert_to_projected(image_path, resampled_image_path, resample=True)
+            convert_to_projected(
+                image_path, resampled_image_path, resample=True, target_gsd_m=args.gsd
+            )
             image_path = resampled_image_path
             assert os.path.exists(image_path)
         else:
             logger.info("Skipping resample")
 
-    predict_and_serialise(
-        image_path,
-        args.instance_seg,
-        os.path.join(args.output, "instance_segmentation"),
-        warm=True,
-    )
+    # if args.skip_predict:
     predict_and_serialise(
         image_path,
         args.semantic_seg,
         os.path.join(args.output, "semantic_segmentation"),
-        warm=True,
+        warm=False,
+        gsd_m=args.gsd,
     )
+    predict_and_serialise(
+        image_path,
+        args.instance_seg,
+        os.path.join(args.output, "instance_segmentation"),
+        warm=False,
+        gsd_m=args.gsd,
+    )
+
     generate_report(image_path, args.output)
 
 
@@ -64,6 +71,8 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--image", help="Input image", required=True)
     parser.add_argument("-o", "--output", help="Working directory", required=True)
     parser.add_argument("-r", "--resample", help="Resample image", action="store_true")
+    # parser.add_argument("--skip_predict", help="Resample image", action="store_false")
+    parser.add_argument("--gsd", type=float, default=0.1)
     parser.add_argument(
         "--instance_seg", help="Resample image", default="config/detectron_tta.yaml"
     )

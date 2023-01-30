a0ec699aefc8cd5ec0c6c3f9bfc0b3a90c3e5860
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2023-01-30 09:00:18 +0100

    clean data cleaning script

--- tools/clean_data.py
+++ tools/clean_data.py
@@ -4,34 +4,37 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from decouple import config
 from PIL import Image
+from pycocotools.coco import COCO
 from tqdm import tqdm
 
-DATA_DIR = config("DATA_DIR")
-C_DATA_DIR = f"{DATA_DIR}corrupted_images/"
+splits = ["train", "val", "test"]
 
-with open(f"{DATA_DIR}val_20221010.json", "r") as file:
-    val_data = json.load(file)
+for split in splits:
 
-ctr = 0
-ids = []
+    annotations = f"data/restor-tcd-oam/{split}_20221010.json"
 
-# check shapes of all images
-print("checking for corrupted images")
-for i, image in enumerate(tqdm(list(val_data.items())[2][1])):
+    dataset = COCO(annotation_file=annotations)
+    empty_images = []
 
-    img_name = list(val_data.items())[2][1][i]["file_name"]
-    img_path = os.path.join(DATA_DIR, "images", img_name)
-    im = np.array(Image.open(img_path))
+    for idx in dataset.imgs:
 
-    if len(im.shape) != 3:
-        ctr += 1
-        ids.append(list(val_data.items())[2][1][i]["id"])
-        if not os.path.exists(C_DATA_DIR):
-            print("Creating folder for corrupted images")
-            os.makedirs(C_DATA_DIR)
-        os.rename(img_path, f"{C_DATA_DIR}{img_name}")
+        anns = dataset.getAnnIds([idx])
+        if len(anns) == 0:
+            # drop image
+            empty_images.append(idx)
 
-print(f"Found and moved {ctr} corrupted images")
-print(f"Removed ids: {ids}")
+    with open(annotations, "r") as fp:
+        dirty_data = json.load(fp)
+
+        for empty_image in empty_images:
+            images = [
+                image
+                for image in dirty_data["images"]
+                if image["id"] not in empty_images
+            ]
+
+        dirty_data["images"] = images
+
+    with open(f"data/restor-tcd-oam/{split}_20221010_noempty.json", "w") as fp:
+        json.dump(dirty_data, fp, indent=1)

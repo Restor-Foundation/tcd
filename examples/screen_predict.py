f765d1ecf32a56c57621714483c9837793eb6d09
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2023-03-11 20:41:34 +0100

    update screen prediction

--- examples/screen_predict.py
+++ examples/screen_predict.py
@@ -1,3 +1,6 @@
+import argparse
+import time
+
 import cv2
 import numpy as np
 import rasterio
@@ -24,12 +27,25 @@ for large images but a modern laptop should be able to handle
 256 x 256 fairly fast.
 """
 
-# Base segmentation model
+# argument parser to switch between models:
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    dest="model",
+    help="choose model to run",
+    type=str,
+    choices=["instance", "semantic"],
+    default="instance",
+)
+args = parser.parse_args()
 
-runner = ModelRunner("config/base_detectron.yaml")
+# Base segmentation model
+if args.model == "semantic":
+    runner = ModelRunner("config/base_semantic_segmentation.yaml")
+elif args.model == "instance":
+    runner = ModelRunner("config/base_detectron.yaml")
 
 # Adjust this to whatever portion of your screen you want to capture.
-mon = {"left": 1024, "top": 256, "width": 1024, "height": 1024}
+mon = {"left": 1300, "top": 512, "width": 1024, "height": 1024}
 
 # Load a dummy image to fake the transform and CRS
 # because we aren't predicting on georeferenced data
@@ -37,7 +53,24 @@ with rasterio.open("data/5c15321f63d9810007f8b06f_10_00000.tif") as src:
     dummy_transform = src.transform
     dummy_crs = src.crs
 
+
+def overlay_two_image(image, overlay, ignore_color=[0, 0, 0]):
+    ignore_color = np.asarray(ignore_color)
+    mask = ~(overlay == ignore_color).all(-1)
+    # Or mask = (overlay!=ignore_color).any(-1)
+    out = image.copy()
+    out[mask] = image[mask] * 0.6 + overlay[mask] * 0.4
+
+    return out
+
+
 with mss() as sct:
+
+    win_name = "Canopy predictions"
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    do_once = True
+    canopy_cover = 0
+
     while True:
         screenShot = sct.grab(mon)
 
@@ -64,23 +97,87 @@ with mss() as sct:
                 dataset.write(img[:, :, i], i + 1)
 
             # Predict, but don't cache!
+            tstart = time.time()
             results = runner.predict(dataset, warm_start=False)
+            # if args.model == "semantic":
+            #    canopy_cover = results.canopy_cover
+            tend = time.time()
+            telapsed = tend - tstart
 
         # thresh_mask = results.confidence_map < 0.25
         # results.confidence_map[thresh_mask] = 0
 
         # Colour map the confidence mask of the image
-        mask = (results.tree_mask * 255.0).astype(np.uint8)
-        mask_coloured = cv2.applyColorMap(mask, cv2.COLORMAP_INFERNO)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        bottomLeftCornerOfText = (10, 1000)
+        fontScale = 1
+        fontColor = (255, 255, 255)
+        thickness = 1
+        lineType = 1
 
-        # Stack with the RGB for visualisation
-        alpha = 0.5
-        beta = 1.0 - alpha
-        combine = cv2.addWeighted(
-            cv2.cvtColor(img, cv2.COLOR_RGB2BGR), alpha, mask_coloured, beta, gamma=0
-        )
+        if args.model == "instance":
+            mask = (results.tree_mask * 255.0).astype(np.uint8)
 
-        cv2.imshow("Canopy predictions", combine)
+            kernel = np.ones((5, 5), np.uint8)
+            mask_erode = cv2.erode(mask, kernel, iterations=1)
+            mask -= mask_erode
+
+            # mask_outline = #fe1493
+            mask_coloured = np.stack((mask,) * 3, axis=-1) * np.array(
+                (147, 20, 254), dtype=mask.dtype
+            )
+
+            combine = overlay_two_image(
+                cv2.cvtColor(img, cv2.COLOR_RGB2BGR), mask_coloured
+            )
+
+            combine = cv2.rectangle(combine, (0, 970), (1024, 1024), (0, 0, 0), -1)
+            cv2.putText(
+                combine,
+                f"Trees detected: {len(results.instances)}, FPS: {1/telapsed:1.2} preds/sec",
+                bottomLeftCornerOfText,
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType,
+            )
+        else:
+            mask = results.confidence_map
+            mask[mask < 0.25] = 0
+
+            canopy_cover = np.count_nonzero(mask) / ((1024 - 64) * (1024 - 64))
+
+            mask_coloured = (np.stack((mask,) * 3, axis=-1) * 255).astype(np.uint8)
+            mask_coloured = cv2.applyColorMap(mask_coloured, cv2.COLORMAP_INFERNO)
+            mask_coloured[mask < 0.25] = (0, 0, 0)
+
+            # Stack with the RGB for visualisation
+            combine = overlay_two_image(
+                cv2.cvtColor(img, cv2.COLOR_RGB2BGR), mask_coloured
+            )
+
+            combine = cv2.rectangle(
+                combine, (64, 64), (1024 - 64, 1024 - 64), (255, 255, 255), 3
+            )
+            combine = cv2.rectangle(combine, (0, 970), (1024, 1024), (0, 0, 0), -1)
+
+            cv2.putText(
+                combine,
+                f"Canopy cover: {canopy_cover:1.2}, FPS: {1/telapsed:1.2} preds/sec",
+                bottomLeftCornerOfText,
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType,
+            )
+
+        cv2.imshow(win_name, combine)
+
+        if do_once:
+            cv2.resizeWindow(win_name, 1024, 1024)
+            do_once = False
         if cv2.waitKey(33) & 0xFF in (
             ord("q"),
             27,

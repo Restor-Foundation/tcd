bf3ae10bae53d200d059f3eef8cf813b474addc1
Josh Veitch-Michaelis <josh@restor.eco>
2023-03-16 03:11:14 +0000

    update training script to use args

--- examples/train_maskrcnn.py
+++ examples/train_maskrcnn.py
@@ -1,8 +1,13 @@
+import argparse
 import os
 
 from tcd_pipeline.modelrunner import ModelRunner
 
 os.environ["WANDB_MODE"] = "disabled"
 
-runner = ModelRunner("config/train_detectron.yaml")
+parser = argparse.ArgumentParser()
+parser.add_argument("config", nargs="?", default="config/train_detectron.yaml")
+args = parser.parse_args()
+
+runner = ModelRunner(args.config)
 runner.train()

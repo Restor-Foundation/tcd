276246c3bbcdbe4573660f94f743e328d8166cbf
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-05-22 17:05:39 +1200

    check repo awk in github actions

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -rf ./docker-tcd-pipeline
-git clone git@github.com:jveitchmichaelis/tcd.git docker-tcd-pipeline --depth 1
+REPO=$(awk '/url/{print $3}' ../.git/config)
+echo $REPO
+git clone $REPO docker-tcd-pipeline --depth 1
 rm -rf docker-tcd-pipeline/.git
 cp -r ../checkpoints ./checkpoints
 docker build -t tcd . -f $1

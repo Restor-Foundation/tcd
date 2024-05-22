b33dc37c6ef25b4d40e3408ff796750d792e3192
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-05-22 17:06:26 +1200

    remove reference to checkpoint folder

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,5 @@ REPO=$(awk '/url/{print $3}' ../.git/config)
 echo $REPO
 git clone $REPO docker-tcd-pipeline --depth 1
 rm -rf docker-tcd-pipeline/.git
-cp -r ../checkpoints ./checkpoints
 docker build -t tcd . -f $1
 rm -rf ./docker-tcd-pipeline
-rm -rf ./checkpoints

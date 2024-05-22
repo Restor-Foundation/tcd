7eaf4ef4d28f8f4fdd8a583132bfb6ac8cdfcafc
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-05-22 16:53:42 +1200

    temp rename repo url

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ./docker-tcd-pipeline
-git clone https://github.com/Restor-Foundation/tcd-pipeline.git docker-tcd-pipeline --depth 1
+git clone git@github.com:jveitchmichaelis/tcd.git docker-tcd-pipeline --depth 1
 rm -rf docker-tcd-pipeline/.git
 cp -r ../checkpoints ./checkpoints
 docker build -t tcd . -f $1

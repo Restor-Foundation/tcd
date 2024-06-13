033dfd75c8805d171994914cd0c601baaeb361e3
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-06-14 03:45:26 +1200

    fix variable typo in hf download tests

--- tests/unit/test_hf_hub_models.py
+++ tests/unit/test_hf_hub_models.py
@@ -23,15 +23,11 @@ def test_load_segformer_models():
 
 
 def test_load_unet_models():
-    for model_id in [
-        "restor/tcd-unet-r34",
-        "restor/tcd-unet-r50",
-        "restor/tcd-unet-r101",
-    ]:
+    for s in [34, 50, 101]:
         api = HfApi()
-        path = api.hf_hub_download(repo_id=model_id, filename="model.pt")
-        smp_unet = smp.Unet(encoder_name="resnet50", classes=2, in_channels=3)
-        model = smp_unet.load_state_dict(torch.load(path), strict=True)
+        path = api.hf_hub_download(repo_id=f"restor/tcd-unet-r{s}", filename="model.pt")
+        model = smp.Unet(encoder_name="resnet" + str(s), classes=2, in_channels=3)
+        model.load_state_dict(torch.load(path), strict=True)
         model.eval()
         dummy_input = torch.rand((1, 3, 512, 512))
         _ = model(dummy_input)

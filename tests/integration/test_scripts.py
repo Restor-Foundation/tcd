4f8154af5fdb1f73ca8d5b1483d05ed97b5cf4b2
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-06-25 16:34:00 +1200

    add script tester

---
+++ tests/integration/test_scripts.py
@@ -0,0 +1,43 @@
+from pytest_console_scripts import ScriptRunner
+
+
+def test_predict_script(script_runner: ScriptRunner):
+    result = script_runner.run(["tcd-predict", "--help"])
+    assert result.returncode == 0
+
+
+def test_predict_script_inference_semantic(script_runner: ScriptRunner):
+    result = script_runner.run(
+        "tcd-predict semantic tests/images/5b3622402b6a08001185f8d8_10_00004.tif output".split(
+            " "
+        )
+    )
+    assert result.returncode == 0
+
+
+def test_predict_script_inference_instance(script_runner: ScriptRunner):
+    result = script_runner.run(
+        "tcd-predict instance tests/images/5b3622402b6a08001185f8d8_10_00004.tif output".split(
+            " "
+        )
+    )
+    assert result.returncode == 0
+
+
+def test_predict_script_inference_zoo(script_runner: ScriptRunner):
+    from tcd_pipeline.pipeline import known_models
+
+    models = known_models.keys()
+
+    for model in models:
+        result = script_runner.run(
+            f"tcd-predict {model} tests/images/5b3622402b6a08001185f8d8_10_00004.tif output".split(
+                " "
+            )
+        )
+        assert result.returncode == 0
+
+
+def test_train_script(script_runner: ScriptRunner):
+    result = script_runner.run(["tcd-train", "--help"])
+    assert result.returncode == 0

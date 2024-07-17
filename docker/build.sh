b1aa2c6efa7c2024b34d5138b4611a8b1ed6fc7d
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2024-07-17 15:39:44 +1200

    rename

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf ./docker-tcd-pipeline
+rm -rf ./docker-tcd
 REPO=$(awk '/url/{print $3}' ../.git/config)
 echo $REPO
-git clone $REPO docker-tcd-pipeline --depth 1
-rm -rf docker-tcd-pipeline/.git
+git clone $REPO docker-tcd --depth 1
+rm -rf docker-tcd/.git
 docker build -t tcd . -f $1
-rm -rf ./docker-tcd-pipeline
+rm -rf ./docker-tcd
